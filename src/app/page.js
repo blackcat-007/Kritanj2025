fbc4017bdad6e0fa8094eabb9719e6b0f70a0f6f
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-03-01 13:59:31 +0530

    the kritanj next js project built and uploaded

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Welcome to Kritanj Next.js Project!</h1>
+    </div>
+  );
+}
