c0d19091bef2598678e1ed9f27c43b69aa5d396a
shivamwebsite <141153430+shivamwebsite@users.noreply.github.com>
2025-03-09 19:23:01 +0530

    SHIVAM

---
+++ src/app/Sponser/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import"./sponsor.css"
+
+function page() {
+    return (
+        <section>
+            <div className="mainbody">
+                <div className='hackonitheader'>Sponsors</div>
+                <div className="hackonitsubhead">Our Sponsors And Partners</div>
+                <div className='border'></div>
+            </div>
+        </section>
+    )
+}
+
+export default page
