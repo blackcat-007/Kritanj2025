dbe5ad5a21a8bb763fc87729217ba944d6d7811e
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-03-12 16:58:18 +0530

    kritanj 2025

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="dark p-0 -mr-6">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <title>Your Page Title</title>
+        <title>Kritanj 2025</title>
       </head>
       <body>
         <main className="app absolute p-0 w-full z-50">
