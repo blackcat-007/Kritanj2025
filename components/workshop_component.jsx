3ef5c37e7d72617c2f760e658c76c4d45500e898
Shubhodeep Mukherjee <shubhodeepmukherjee24@gmail.com>
2025-03-14 09:46:52 +0530

    workshop updated

--- components/workshop_component.jsx
+++ components/workshop_component.jsx
@@ -5,22 +5,28 @@ import { faCircleUp } from '@fortawesome/free-regular-svg-icons';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { useSwipeable } from 'react-swipeable';
 const events = [
-  { id: 1, contestname: 'APT venture', poster: '/aptworkshop.jpg' },
-  { id: 2, contestname: 'Robotics', poster: '/roboticsworkshop.jpg' },
-  { id: 3, contestname: 'Management', poster: '/managementworkshop.jpg' },
-
+  { id: 1, contestname: 'APT venture', poster: '/aptworkshop.jpg',instalink:'https://www.instagram.com/p/DG8j1tSyjw8/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' },
+  { id: 2, contestname: 'Robotics', poster: '/roboticsworkshop.jpg',instalink:'https://www.instagram.com/p/DGz63gaSghX/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' },
+  { id: 3, contestname: 'Management', poster: '/managementworkshop.jpg',instalink:'https://www.instagram.com/p/DG46a1qSQzl/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==' },
+  { id: 4, contestname: 'Code War', poster: '/codingworkshop.jpg',instalink:'https://www.instagram.com/p/DGxuhHJSVAG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D' },
+  { id: 5, contestname: 'Elektron Pro', poster: '/elektronproworkshop.jpg',instalink:'https://www.instagram.com/p/DGvtC6dSR8q/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D' },
+  { id: 6, contestname: 'Civil Creation', poster: '/civilcreationworkshop.jpg',instalink:'https://www.instagram.com/p/DHAdkWPytKr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D' },
+  { id: 7, contestname: 'Mech Tech ', poster: '/mechtechworkshop.jpg',instalink:'https://www.instagram.com/p/DG4_aj1yh7l/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D' },
+  { id: 8, contestname: 'Elektron Lite', poster: '/elektronliteworkshop.jpg',instalink:'https://www.instagram.com/p/DG0P80oyvuY/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D' },
+  
 ];
 
-const EventCard = ({ title, image }) => (
+const EventCard = ({ title, image,link }) => (
   <div className="w-full sm:w-72 h-52 md:h-96 bg-black rounded-2xl shadow-lg overflow-hidden relative group">
     <img  
       src={image} 
       alt={title} 
       className="w-full h-full object-cover transition-transform duration-500 ease-in-out transform group-hover:scale-110" 
     />
+
     <div 
       className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white rounded-xl h-auto text-black w-3/4 md:w-auto flex items-start md:items-center justify-between p-2 sm:p-4 shadow-md cursor-pointer"
-      onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSfruZFRAC35X3qbS8TRB6mDZEQIS_iVp2xdyro8Abf0ukI-OQ/viewform', '_blank')}
+      onClick={() => window.open(link, '_blank')}
     >
       <h2 className="text-xs sm:text-sm md:text-lg md:font-semibold leading-tight md:leading-normal">
         {title}
@@ -94,7 +100,7 @@ export default function Event() {
             </button>
           )}
           {events.slice(index, index + cardsPerSlide).map((event) => (
-            <EventCard key={event.id} title={event.contestname} image={event.poster} />
+            <EventCard key={event.id} title={event.contestname} image={event.poster} link={event.instalink} />
           ))}
           {index + cardsPerSlide < events.length && (
             <button 
