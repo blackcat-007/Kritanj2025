bf62c12bb011ecfb57c31cd09ca142edcbd7d98f
shivamwebsite <141153430+shivamwebsite@users.noreply.github.com>
2025-03-15 21:04:54 +0530

    sponsor

--- src/app/Sponser/page.js
+++ src/app/Sponser/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import"./sponsor.css"
+import "./sponsor.css"
 
 function page() {
     return (
@@ -8,6 +8,19 @@ function page() {
                 <div className='hackonitheader'>Sponsors</div>
                 <div className="hackonitsubhead">Our Sponsors And Partners</div>
                 <div className='border'></div>
+                <div className="sponsorheader">TECHNICAL COLLABORATION</div>
+                <div className="sponsorimage">
+                    <span><img src="/TECH-IMG.png" alt="error" /></span>
+                    <span><img src="/TECH-IMG2.png" alt="" /></span>
+                </div>
+                <div className="sponsorheader">IN ASSOCIATION WITH</div>
+                <div className="sponsorimage">
+                    <img src="/Asso-img.png" alt="error" />
+                </div>
+                <div className="sponsorheader">POWERED BY</div>
+                <div className="sponsorimage">
+                    <span><img src="/powered-img.jpg" alt="error" /></span>
+                </div>
             </div>
         </section>
     )
